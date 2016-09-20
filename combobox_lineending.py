772cf3bd519864d563d1ff49cec5c7d1f7344f2d
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-20 18:27:43 +0200

    create `ComboBox_LineEnding` class for `line_ending_combobox`

---
+++ combobox_lineending.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QComboBox
+
+class ComboBox_LineEnding(QComboBox):
+	"""docstring for QComboBox"""
+	def __init__(self, parent):
+		super(ComboBox_LineEnding, self).__init__(parent)
+		self.parent = parent
+		
