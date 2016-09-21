d8c86722b45d503f63e3fa2fd24d8dd676508afa
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-21 18:24:45 +0200

    Signals and slot: `line_ending_combobox` and `serial_input`

--- lineedit_serialin.py
+++ lineedit_serialin.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import pyqtSignal
 class LineEdit_SerialIn(QLineEdit):
 	"""docstring for LineEdit_SerialIn"""
 
+	__line_ending = "\n"
+
 	# create a signal
 	input_sent = pyqtSignal(['QString'])
 
@@ -12,5 +14,19 @@ class LineEdit_SerialIn(QLineEdit):
 		self.parent = parent
 		
 	def on_return_pressed(self):
-		self.input_sent.emit(self.text())
+		text = self.text() + self.__line_ending
+		self.input_sent.emit(text)
 		self.clear()
+
+	def on_line_ending_changed(self, line_ending_index):
+		if line_ending_index == 0:
+			self.__line_ending = ""
+
+		elif line_ending_index == 1:
+			self.__line_ending = "\n"
+
+		elif line_ending_index == 2:
+			self.__line_ending = "\r"
+
+		elif line_ending_index == 3:
+			self.__line_ending = "\r\n"
