5036eba64f1b5a47ee2fc4884a22509dfb27fa5c
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-21 04:01:06 +0200

    Signals and slot: `LineEdit_SerialIn` and `SerialOut_TextEdit`

--- lineedit_serialin.py
+++ lineedit_serialin.py
@@ -1,8 +1,17 @@
 from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtCore import pyqtSignal
 
 class LineEdit_SerialIn(QLineEdit):
 	"""docstring for LineEdit_SerialIn"""
+
+	# create a signal
+	input_sent = pyqtSignal(['QString'])
+
 	def __init__(self, parent):
 		super(LineEdit_SerialIn, self).__init__(parent)
 		self.parent = parent
 		
+	def on_return_pressed(self):
+		if self.text() != "":
+			self.input_sent.emit(self.text())
+		self.clear()
--- serialout_textedit.py
+++ serialout_textedit.py
@@ -1,8 +1,11 @@
 from PyQt5.QtWidgets import QTextEdit
+# from PyQt5.QtCore import pyqtSlot
 
 class SerialOut_TextEdit(QTextEdit):
 	"""docstring for SerialOut_TextEdit"""
 	def __init__(self, parent):
 		super(SerialOut_TextEdit, self).__init__(parent)
 		self.parent = parent
-		
+
+	def on_input_sent(self, text):
+		self.append(text)
