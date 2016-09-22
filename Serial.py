4460cb022eec7619e13f51ba3fca9e399f3c2068
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-22 11:53:25 +0200

    - create `SerialInfo` class - create `get_available_ports_systemLocations_and_manufactures` method

--- Serial.py
+++ Serial.py
@@ -1,7 +1,7 @@
-from PyQt5.QtSerialPort import QSerialPort
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 
 class Serial(QSerialPort):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super(Serial, self).__init__(parent)
         self.parent = parent
         self.setBaudRate = self.Baud9600
@@ -9,3 +9,17 @@ class Serial(QSerialPort):
         self.setParity = self.NoParity
         self.setStopBits = self.OneStop
         self.setFlowControl = self.HardwareControl
+
+
+class SerialInfo(QSerialPortInfo):
+    def __init__(self, parent=None):
+        super(SerialInfo, self).__init__(parent)
+
+    def get_available_ports_systemLocations_and_manufacturers(self):
+        available_ports = self.availablePorts()
+        ports_info = []
+
+        for port in available_ports:
+            ports_info.append( (port.systemLocation(), port.manufacturer()) )
+
+        return ports_info
