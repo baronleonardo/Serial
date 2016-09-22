108d99acd62a9779c7cb1fddd72e53ff3b0d4a30
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-22 17:33:59 +0200

    remove `SerialInfo` class and use `get_available_ports_systemLocations_and_manufacturers` in `Serial` class

--- Serial.py
+++ Serial.py
@@ -10,13 +10,8 @@ class Serial(QSerialPort):
         self.setStopBits = self.OneStop
         self.setFlowControl = self.HardwareControl
 
-
-class SerialInfo(QSerialPortInfo):
-    def __init__(self, parent=None):
-        super(SerialInfo, self).__init__(parent)
-
     def get_available_ports_systemLocations_and_manufacturers(self):
-        available_ports = self.availablePorts()
+        available_ports = QSerialPortInfo.availablePorts()
         ports_info = []
 
         for port in available_ports:
