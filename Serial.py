c9486017345c417e836ad30b6533571a00f2ac70
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-22 23:57:35 +0200

    Solve a bug: rename super-class variables correctly

--- Serial.py
+++ Serial.py
@@ -3,12 +3,12 @@ from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 class Serial(QSerialPort):
     def __init__(self, parent=None):
         super(Serial, self).__init__(parent)
-        self.parent = parent
-        self.setBaudRate = self.Baud9600
-        self.setDataBits = self.Data8
-        self.setParity = self.NoParity
-        self.setStopBits = self.OneStop
-        self.setFlowControl = self.HardwareControl
+        self.parent      = parent
+        self.baudRate    = self.Baud9600
+        self.dataBits    = self.Data8
+        self.parity      = self.NoParity
+        self.stopBits    = self.OneStop
+        self.flowControl = self.NoFlowControl
 
     def get_available_ports_systemLocations_and_manufacturers(self):
         available_ports = QSerialPortInfo.availablePorts()
