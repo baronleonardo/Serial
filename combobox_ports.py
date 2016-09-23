c83fd7f03186974bd85e1095ef16c13c7ae426a2
Sara ElDeeb <saraadel.fcis2012@gmail.com>
2016-09-23 22:05:00 +0200

    create a function to update the ports info list

--- combobox_ports.py
+++ combobox_ports.py
@@ -1,8 +1,22 @@
 from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtCore import Qt
 
 class ComboBox_Ports(QComboBox):
-	"""docstring for LineEdit_SerialIn"""
+	"""docstring for ComboBox_Ports"""
 
 	def __init__(self, parent=None):
 		super(ComboBox_Ports, self).__init__(parent)
 		self.parent = parent
+
+
+	def updateList(self):
+
+		# TODO: call function to get the "port info" list
+		lst = []
+
+		for index in range(0, len(lst)):
+			self.addItem(lst[index][1])
+			self.setItemData(index, lst[index][0], Qt.ToolTipRole)
+			print(self.currentIndex())
+
+		return
