91c142d5c8a85420c3bee97a81cad12705854997
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-24 22:14:52 +0200

    add `gtk2` style due to its absence in PyQt5

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys, os
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QCoreApplication
 from MainWindow import Ui
 
 if __name__ == '__main__':
@@ -9,4 +10,9 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     window = Ui(executable_path)
+
+    library_paths = QCoreApplication.libraryPaths()
+    QCoreApplication.setLibraryPaths([executable_path] + library_paths)
+    app.setStyle( "gtk2" )
+
     sys.exit(app.exec_())
