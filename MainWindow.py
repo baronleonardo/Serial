7490afa07a5baded110442a591d4f10173b3bf84
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-25 13:00:46 +0200

    signal and slot: choose baudrate -> change the baud rate in Serial

--- MainWindow.py
+++ MainWindow.py
@@ -2,15 +2,20 @@ from PyQt5 import uic
 from PyQt5.QtChart import QChartView
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QDir
+from Serial import Serial
 
 class Ui(QMainWindow):
     def __init__(self, executable_path):
         super(Ui, self).__init__()
         ui_file = executable_path + QDir.separator() + 'Serial.ui'
         uic.loadUi(ui_file, self)
+        self.serial = Serial()
         self.__signals()
         self.show()
     
     def __signals(self):
         self.page_forward_btn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
         self.page_backward_btn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page))
+
+        # choose baudrate -> change the baud rate in Serial
+        self.baudrate_combobox.currentTextChanged.connect( self.serial.setBaudRate_str )
