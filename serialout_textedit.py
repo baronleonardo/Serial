c5388e96ea0b75822359d2b34c2ae880cddebc24
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-25 16:58:09 +0200

    `on_serial_read` method now can handle `str` and `bytes`

--- serialout_textedit.py
+++ serialout_textedit.py
@@ -1,14 +1,16 @@
 from PyQt5.QtWidgets import QTextEdit
-# from PyQt5.QtCore import pyqtSlot
 
 class SerialOut_TextEdit(QTextEdit):
-	"""docstring for SerialOut_TextEdit"""
-	def __init__(self, parent=None):
-		super(SerialOut_TextEdit, self).__init__(parent)
-		self.parent = parent
+    """docstring for SerialOut_TextEdit"""
+    def __init__(self, parent=None):
+        super(SerialOut_TextEdit, self).__init__(parent)
+        self.parent = parent
 
-	def on_serial_read(self, text):
-		self.append(text)
+    def on_serial_read(self, text):
+        if type(text) is bytes:
+            self.append(text.decode('ascii'))
+        elif type(text) is str:
+            self.append(text)
 
-	def on_autoscroll_state_changed(self, state):
-		pass
+    def on_autoscroll_state_changed(self, state):
+        pass
