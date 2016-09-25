5b40d957de0f87dca0e00c2b1891a6d7aafe9a35
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-26 01:53:03 +0200

    autscroll now functional

--- serialout_textedit.py
+++ serialout_textedit.py
@@ -1,14 +1,24 @@
 from PyQt5.QtWidgets import QTextEdit
-# from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import Qt
 
 class SerialOut_TextEdit(QTextEdit):
-	"""docstring for SerialOut_TextEdit"""
-	def __init__(self, parent=None):
-		super(SerialOut_TextEdit, self).__init__(parent)
-		self.parent = parent
+    """docstring for SerialOut_TextEdit"""
+    def __init__(self, parent=None):
+        super(SerialOut_TextEdit, self).__init__(parent)
+        self.parent = parent
 
-	def on_serial_read(self, text):
-		self.append(text)
+        self.__v_scrollbar = self.verticalScrollBar()
+        self.cursorPositionChanged.connect(self.onCursorPositionChanged)
 
-	def on_autoscroll_state_changed(self, state):
-		pass
+    def on_serial_read(self, text):
+        self.append(text)
+
+    def onCursorPositionChanged(self):
+        self.__v_scrollbar.setSliderPosition(self.__v_scrollbar.maximum())
+
+    def on_autoscroll_state_changed(self, state):
+        if state == Qt.Checked:
+            self.cursorPositionChanged.connect(self.onCursorPositionChanged)
+
+        elif state == Qt.Unchecked:
+            self.cursorPositionChanged.disconnect(self.onCursorPositionChanged)
