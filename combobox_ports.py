e463dffc471483b12bb103e99e1b80cb892295d0
Sara ElDeeb <saraadel.fcis2012@gmail.com>
2016-09-26 18:41:55 +0200

    make variable lst global to be used in other functions

--- combobox_ports.py
+++ combobox_ports.py
@@ -2,8 +2,11 @@ from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtCore import Qt
 from Serial import Serial
 
+
 class ComboBox_Ports(QComboBox):
 	"""docstring for ComboBox_Ports"""
+
+	lst = []
 
 	def __init__(self, parent=None):
 		super(ComboBox_Ports, self).__init__(parent)
@@ -11,11 +14,11 @@ class ComboBox_Ports(QComboBox):
 
 	def updateList(self):
 
-		lst = Serial.get_available_ports_systemLocations_and_manufacturers()
+		self.lst = Serial.get_available_ports_systemLocations_and_manufacturers()
 
-		for index in range(0, len(lst)):
-			self.addItem(lst[index][1])
-			self.setItemData(index, lst[index][0], Qt.ToolTipRole)
+		for index in range(0, len(self.lst)):
+			self.addItem(self.lst[index][1])
+			self.setItemData(index, self.lst[index][0], Qt.ToolTipRole)
 
 	def showPopup(self):
 		self.clear()
