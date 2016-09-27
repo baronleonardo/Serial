76fcd4d0ca78a8dac67c63e961b2450708ef6d08
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-27 13:00:55 +0200

    create `Chart` class

--- chartview.py
+++ chartview.py
@@ -1,8 +1,12 @@
-from PyQt5.QtChart import QChartView
+from PyQt5.QtChart import QChartView, QChart
 
 class ChartView(QChartView):
-	"""docstring for ChartView"""
-	def __init__(self, parent=None):
-		super(ChartView, self).__init__(parent)
-		self.parent = parent
-		
+    """docstring for ChartView"""
+    def __init__(self, parent=None):
+        super(ChartView, self).__init__(parent)
+        self.parent = parent
+        
+class Chart(QChart):
+    def __init__(self, parent=None):
+        super(Chart, self).__init__(parent)
+        self.parent = parent
