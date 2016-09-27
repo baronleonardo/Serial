52cfca760365b6b9980ebbde11482c9ec591350b
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-27 19:31:22 +0200

    add properties of `Chart`

--- chartview.py
+++ chartview.py
@@ -10,7 +10,24 @@ class Chart(QChart):
     def __init__(self, parent=None):
         super(Chart, self).__init__(parent)
         self.parent = parent
+        # we will draw lines
         self.series = QLineSeries(parent)
+        # color and pen-width
+        self.series.setPen(QPen(Qt.red, 1))
+        self.addSeries(self.series)
+
+        self.legend().hide()
+        self.__construct_axises()
 
     def add_point(self, x ,y):
         self.series.append(x, y)
+
+    def __construct_axises(self):
+        self.createDefaultAxes()
+
+        # X-Axis
+        x_axis = self.axisX()
+        x_axis.hide()
+
+        # Y-axis
+        y_axis = self.axisY()
