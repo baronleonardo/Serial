7e0dc8536b35233d68d60a77e1e117d89842ef5c
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-27 13:03:40 +0200

    add `add_point` method

--- chartview.py
+++ chartview.py
@@ -1,4 +1,4 @@
-from PyQt5.QtChart import QChartView, QChart
+from PyQt5.QtChart import QChartView, QChart, QLineSeries
 
 class ChartView(QChartView):
     """docstring for ChartView"""
@@ -10,3 +10,7 @@ class Chart(QChart):
     def __init__(self, parent=None):
         super(Chart, self).__init__(parent)
         self.parent = parent
+        self.series = QLineSeries(parent)
+
+    def add_point(self, x ,y):
+        self.series.append(x, y)
