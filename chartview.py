26976dfd57ccb583a729aa3ae226ee66975bfaa7
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-09-30 11:57:31 +0200

    add important methods to `Chart` class

--- chartview.py
+++ chartview.py
@@ -15,20 +15,57 @@ class ChartView(QChartView):
         self.chart.add_point(x , y)
         
 class Chart(QChart):
+    MIN_X = 0
+    MAX_X = 750
+    MIN_Y = -10
+    MAX_Y = 10
+    TICKS = 3
+    PENCOLOR = Qt.red
+    PENWIDTH = 1
+
     def __init__(self, parent=None):
         super(Chart, self).__init__(parent)
         self.parent = parent
         # we will draw lines
         self.series = QLineSeries(parent)
         # color and pen-width
-        self.series.setPen(QPen(Qt.red, 1))
+        self.series.setPen(QPen(self.PENCOLOR, self.PENWIDTH))
+
         self.addSeries(self.series)
 
         self.legend().hide()
         self.__construct_axises()
 
+    def setRange_X_axis(self, min_X, max_X):
+        self.MIN_X = min_X
+        self.MAX_X = max_X
+        self.axisX().setRange(self.MIN_X, self.MAX_X)
+
+    def setRange_Y_axis(self, min_Y, max_Y):
+        self.MIN_Y = min_Y
+        self.MAX_Y = max_Y
+        self.axisY().setRange(self.MIN_Y, self.MAX_Y)
+
     def add_point(self, x ,y):
         self.series.append(x, y)
+
+    def get_series(self) -> list:
+        return self.series.pointsVector()
+
+    def remove_point(self, index):
+        self.series.remove(index)
+
+    def remove_points(self, index, count):
+        self.series.removePoints(index, count)
+
+    def replace_point(self, index, x, y):
+        self.series.replace(index , x , y)
+
+    def replace_series(self, lst):
+        self.series.replace(lst)
+
+    def get_series_count(self):
+        return self.series.count()
 
     def __construct_axises(self):
         self.createDefaultAxes()
@@ -36,6 +73,9 @@ class Chart(QChart):
         # X-Axis
         x_axis = self.axisX()
         x_axis.hide()
+        x_axis.setRange(self.MIN_X, self.MAX_X)
 
         # Y-axis
         y_axis = self.axisY()
+        y_axis.setRange(self.MIN_Y, self.MAX_Y)
+        y_axis.setTickCount(self.TICKS)
