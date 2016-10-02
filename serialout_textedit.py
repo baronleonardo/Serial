80875c07ad6d5fe254b3bb5002ab593a2dfbb28e
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-10-02 13:11:12 +0200

    Solve a bug: `QTextEdit` maybe has bug, as it treats `\r` as new line

--- serialout_textedit.py
+++ serialout_textedit.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtCore import Qt
+import platform
 
 class SerialOut_TextEdit(QTextEdit):
     """docstring for SerialOut_TextEdit"""
@@ -9,9 +10,11 @@ class SerialOut_TextEdit(QTextEdit):
 
         self.__v_scrollbar = self.verticalScrollBar()
         self.cursorPositionChanged.connect(self.onCursorPositionChanged)
+        self.OS_TYPE = platform.system()
 
     def on_serial_read(self, text):
         self.append(text)
+        if text != '\r' or self.OS_TYPE == 'Windows':
 
     def onCursorPositionChanged(self):
         self.__v_scrollbar.setSliderPosition(self.__v_scrollbar.maximum())
