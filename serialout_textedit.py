5694e10d3245219b85b8b4ee547717fe5e0ad9f7
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-10-03 23:09:25 +0200

    adopt serial out read method, to read correctly

--- serialout_textedit.py
+++ serialout_textedit.py
@@ -13,23 +13,22 @@ class SerialOut_TextEdit(QTextEdit):
         self.is_auto_scrolling = True
 
     def on_serial_read(self, text):
-        if text != '\r' or self.OS_TYPE == 'Windows':
-            prev_cursor_pos = self.textCursor()
-            prev_v_scrollbar_pos = self.verticalScrollBar().value()
-            prev_h_scrollbar_pos = self.horizontalScrollBar().value()
+        prev_cursor_pos = self.textCursor()
+        prev_v_scrollbar_pos = self.verticalScrollBar().value()
+        prev_h_scrollbar_pos = self.horizontalScrollBar().value()
 
+        self.moveCursor(QTextCursor.End)
+        self.insertPlainText(text)
+
+        if self.is_auto_scrolling is False:
+            self.setTextCursor(prev_cursor_pos)
+            self.verticalScrollBar().setValue(prev_v_scrollbar_pos)
+            self.horizontalScrollBar().setValue(prev_h_scrollbar_pos)
+
+        elif self.is_auto_scrolling is True:
             self.moveCursor(QTextCursor.End)
-            self.insertPlainText(text)
-
-            if self.is_auto_scrolling is False:
-                self.setTextCursor(prev_cursor_pos)
-                self.verticalScrollBar().setValue(prev_v_scrollbar_pos)
-                self.horizontalScrollBar().setValue(prev_h_scrollbar_pos)
-
-            elif self.is_auto_scrolling is True:
-                self.moveCursor(QTextCursor.End)
-                self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
-                self.horizontalScrollBar().setValue(self.horizontalScrollBar().maximum())
+            self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+            self.horizontalScrollBar().setValue(self.horizontalScrollBar().maximum())
 
     def on_autoscroll_state_changed(self, state):
         if state == Qt.Checked:
